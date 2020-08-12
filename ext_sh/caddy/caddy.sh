23b58678916e58d7e6146ee7e5809f48ac91a4ea
tcneko <tcneko@outlook.com>
2020-08-12 15:30:41 +0800

    v20200812_6

--- ext_sh/caddy/caddy.sh
+++ ext_sh/caddy/caddy.sh
@@ -10,6 +10,7 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 # variables
 d_cur="$(dirname ${BASH_SOURCE[0]})"
 f_cfg="${d_cur}/caddy_cfg.sh"
+d_caddy="/etc/caddy"
 
 # function
 load_cfg() {
@@ -24,6 +25,9 @@ ins_caddy() {
   echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" > /etc/apt/sources.list.d/caddy-fury.list
   apt update
   apt install caddy
+  test_or_mkdir ${d_caddy}
+  cp -f ${d_cur}/caddyenv ${d_caddy}/
+  touch ${d_caddy}/caddyfile
   cp -f ${d_cur}/caddy.service /lib/systemd/system/
 }
 
