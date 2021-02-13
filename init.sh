07d0827886cb16312184c7f85f226d204d600160
tcneko <tcneko@outlook.com>
2021-02-13 22:41:47 +0800

    v20210213_2

--- init.sh
+++ init.sh
@@ -8,36 +8,53 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 # variables
+d_cur="$(dirname ${BASH_SOURCE[0]})"
+f_cfg="${d_cur}/arashi.json"
+f_lib="${d_cur}/lib/lib_arashi.sh"
 d_ins='/opt/sh/arashi'
 
 # function
-echo_info() {
-  echo -e "\e[1;32m[Info]\e[0m $@"
+load_lib() {
+  if [[ -r ${f_lib} ]]; then
+    source ${f_lib}
+  else
+    exit 1
+  fi
 }
 
-echo_error() {
-  echo >&2 -e "\e[1;31m[Error]\e[0m $@"
+check_root_exit() {
+  if (($(id -u) == 0)); then
+    echo_error 'Please rerun as root'
+    exit 1
+  fi
+}
+
+check_command_exit() {
+  command -v jq &>/dev/null
+  if (($? == 0)); then
+    echo_error "Please install \"jq\""
+    exit 1
+  fi
+}
+
+install_arashi() {
+  mkdir -p ${d_ins}
+
+  cp -rf arashi.sh arashi.json task ${d_ins}
+  cp -rf version "${d_ins}/version_arashi"
+
+  echo_info 'Init succeeded if no error is reported above'
+  echo_info "Please run: sudo bash ${d_ins}/arashi.sh"
+}
+
+main() {
+  load_lib
+  check_root_exit
+  check_command_exit
+  install_arashi
 }
 
 # main
-if (($(id -u) == 0)); then
-  echo_error 'Please rerun as root'
-  exit 1
-fi
-
-command -v jq &>/dev/null
-if (($? == 0)); then
-  echo_error "Please install \"jq\""
-  exit 1
-fi
-
-if [ ! -d ${d_ins} ]; then
-  mkdir -p ${d_ins}
-fi
-cp -rf *.sh base_sh ext_sh ${d_ins}
-cp -rf version "${d_ins}/version_arashi"
-
-echo_info 'Init succeeded if no error is reported above'
-echo_info "Please run: sudo bash ${d_ins}/arashi.sh"
+main
 
 exit 0
